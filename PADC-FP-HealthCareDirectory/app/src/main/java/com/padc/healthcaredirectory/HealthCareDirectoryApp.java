03d7475b859f1dbfe2a01ed24ec0251b414595c9
Phyoe Khant <pisces.pkz@gmail.com>
2016-09-04 15:45:32 -0700

    initial Android Project Set up

---
+++ PADC-FP-HealthCareDirectory/app/src/main/java/com/padc/healthcaredirectory/HealthCareDirectoryApp.java
@@ -0,0 +1,23 @@
+package com.padc.healthcaredirectory;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by Phyoe Khant on 9/4/2016.
+ */
+public class HealthCareDirectoryApp extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
