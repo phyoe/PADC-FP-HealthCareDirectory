45922bd9429d1517e5f1f60f295f9454aeddd84b
SawYuNwe <thautansaw@gmail.com>
2016-09-07 11:24:11 -0600

    Creating Detail  Activity for hospital, clinic and phamacy

---
+++ PADC-FP-HealthCareDirectory/app/src/main/java/com/padc/healthcaredirectory/activities/PhamacyDetailActivity.java
@@ -0,0 +1,15 @@
+package com.padc.healthcaredirectory.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.padc.healthcaredirectory.R;
+
+public class PhamacyDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phamacy_detail);
+    }
+}
---
+++ PADC-FP-HealthCareDirectory/app/src/main/java/com/padc/healthcaredirectory/components/DynamicHeightImageView.java
@@ -0,0 +1,32 @@
+package com.padc.healthcaredirectory.components;
+
+/**
+ * Created by Saw Yu Nwe on 9/6/2016.
+ */
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class DynamicHeightImageView extends ImageView {
+    private float mAspectRatio = 1.4f;
+
+    public DynamicHeightImageView(Context context) {
+        super(context);
+    }
+
+    public DynamicHeightImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setAspectRatio(float aspectRatio) {
+        mAspectRatio = aspectRatio;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
+    }
+}
